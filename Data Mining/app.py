78d179f93eaa621d7e91d9c3c11b99fed27f4655
marco12893 <143802372+marco12893@users.noreply.github.com>
2024-11-27 16:51:21 +0700

    Add files via upload

---
+++ Data Mining/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    print("Flask app running...")
+    return render_template('index.html', title="Home", header="Welcome to Flask")
+
+@app.route('/about')
+def about():
+    print("Flask app running...")
+    return render_template('base.html', title="About", header="About Flask")
+
+if __name__ == '__main__':
+    app.run(debug=True)
